91064efc67f6b8eecbded34901dbd67b5ce1eac9
ezex87 <ezequielmiranda87@gmail.com>
2019-03-06 10:01:57 +0100

    Updates

---
+++ precache-manifest.aff0f318df968ef6b4484e47fb6721bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6717559e02bb851691e19132519e1f61",
+    "url": "/react-post-board/static/media/post-logo.6717559e.png"
+  },
+  {
+    "revision": "becc3f8f857f4886ca3a",
+    "url": "/react-post-board/static/js/runtime~main.becc3f8f.js"
+  },
+  {
+    "revision": "c358445e294192b7c698",
+    "url": "/react-post-board/static/js/main.c358445e.chunk.js"
+  },
+  {
+    "revision": "bc63fde73f0b0a4eefab",
+    "url": "/react-post-board/static/js/2.bc63fde7.chunk.js"
+  },
+  {
+    "revision": "c358445e294192b7c698",
+    "url": "/react-post-board/static/css/main.94eb4880.chunk.css"
+  },
+  {
+    "revision": "691e036e6ecbb6fe888a3768155805c2",
+    "url": "/react-post-board/index.html"
+  }
+];
